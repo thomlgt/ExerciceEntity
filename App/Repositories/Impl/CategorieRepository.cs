9237ea7f31b2594619e8ec9fdd5651528402b0a1
Thomas.L <thomlgt@hotmail.fr>
2020-03-25 12:32:35 +0100

    Ajout CategorieRepository et MaterielHasFicheRepository

---
+++ App/Repositories/Impl/CategorieRepository.cs
@@ -0,0 +1,69 @@
+﻿using App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Repositories.Impl
+{
+    public class CategorieRepository : IRepository<Categorie>
+    {
+        private MedicalContext context;
+
+        public CategorieRepository(MedicalContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Categorie> FindAll()
+        {
+            return context.Categorie;
+        }
+        public Categorie FindById(int id)
+        {
+            return context.Categorie.Find(id);
+        }
+
+        public void Insert(Categorie categorie)
+        {
+            context.Categorie.Add(categorie);
+        }
+
+        public void Delete(int id)
+        {
+            Categorie categorie = context.Categorie.Find(id);
+            context.Categorie.Remove(categorie);
+        }
+
+        public void Update(Categorie categorie)
+        {
+            context.Entry(categorie).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ App/Repositories/Impl/MaterielHasFicheRepository.cs
@@ -0,0 +1,69 @@
+﻿using App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Repositories.Impl
+{
+    public class MaterielHasFicheRepository : IRepository<MaterielHasFiche>
+    {
+        private MedicalContext context;
+
+        public MaterielHasFicheRepository(MedicalContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<MaterielHasFiche> FindAll()
+        {
+            return context.MaterielHasFiche;
+        }
+        public MaterielHasFiche FindById(int id)
+        {
+            return context.MaterielHasFiche.Find(id);
+        }
+
+        public void Insert(MaterielHasFiche data)
+        {
+            context.MaterielHasFiche.Add(data);
+        }
+
+        public void Delete(int id)
+        {
+            MaterielHasFiche data = context.MaterielHasFiche.Find(id);
+            context.MaterielHasFiche.Remove(data);
+        }
+
+        public void Update(MaterielHasFiche data)
+        {
+            context.Entry(data).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
