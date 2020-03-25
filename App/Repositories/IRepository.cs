b32c3fc0a2b598877a9419bd79358dd26aa4365e
Thomas.L <thomlgt@hotmail.fr>
2020-03-25 12:04:54 +0100

    Ajout repository generique

---
+++ App/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public interface IRepository<T> : IDisposable
+    {
+        public IEnumerable<T> FindAll();
+        public T FindById(int id);
+        public void Insert(T model);
+        public void Delete(int id);
+        public void Update(T model);
+        public void Save();
+    }
+}
--- App/Repositories/Impl/ClientRepository.cs
+++ App/Repositories/Impl/ClientRepository.cs
@@ -1,11 +1,69 @@
-﻿using System;
+﻿using App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace App.Repositories.Impl
 {
-    public class ClientRepository : IClientRepository
+    public class ClientRepository : IRepository<Client>
     {
+        private MedicalContext context;
+
+        public ClientRepository(MedicalContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Client> FindAll()
+        {
+            return context.Client;
+        }
+        public Client FindById(int id)
+        {
+            return context.Client.Find(id);
+        }
+
+        public void Insert(Client client)
+        {
+            context.Client.Add(client);
+        }
+
+        public void Delete(int id)
+        {
+            Client client = context.Client.Find(id);
+            context.Client.Remove(client);
+        }
+
+        public void Update(Client client)
+        {
+            context.Entry(client).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
